86c9c2cddc75903ea7c1a2fc11c2e62cf4429554
Trang Huffman <113157098+tranghuffman@users.noreply.github.com>
2023-03-05 10:44:30 -0500

    Add files via upload

---
+++ python.py
@@ -0,0 +1,13 @@
+class do_math:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+    
+    def subtraction(self):
+        return self.num1 - self.num2
+    def division(self):
+        return self.num1 // self.num2
+
+math_instance = do_math(15, 5)
+print(math_instance.subtraction()) 
+print(math_instance.division())
